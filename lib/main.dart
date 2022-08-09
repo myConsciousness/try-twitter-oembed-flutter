c733e8df15e5ca7af7b80562bd8531cea0329653
myConsciousness <kato.shinya.dev@gmail.com>
2022-08-09 19:13:10 +0900

    update

--- lib/main.dart
+++ lib/main.dart
@@ -40,6 +40,9 @@ class MyApp extends StatelessWidget {
 
   void _loadEmbeddedTimeline() {
     String fileText = '''
+<blockquote class="twitter-tweet"><p lang="en" dir="ltr">Free AI for Beginners Course - Microsoft has put together an AI course for beginners, consisting of a 12 week, 24 lesson curriculum, available for free to all<a href="https://twitter.com/hashtag/AI?src=hash&amp;ref_src=twsrc%5Etfw">#AI</a> <a href="https://twitter.com/hashtag/course?src=hash&amp;ref_src=twsrc%5Etfw">#course</a> <a href="https://twitter.com/hashtag/KDnuggets?src=hash&amp;ref_src=twsrc%5Etfw">#KDnuggets</a> <a href="https://t.co/kxrEOQ07N5">https://t.co/kxrEOQ07N5</a> <a href="https://t.co/VJx2BkXkS9">pic.twitter.com/VJx2BkXkS9</a></p>&mdash; KDnuggets (@kdnuggets) <a href="https://twitter.com/kdnuggets/status/1556626379860475906?ref_src=twsrc%5Etfw">August 8, 2022</a></blockquote>
+<script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+
     <a
       class="twitter-timeline"
       href="https://twitter.com/Twiterdev?ref_src=twsrc%5Etfw"
